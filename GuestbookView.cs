1df2ce3af3dffe688f08ee7ee1292319dc886e7b
KAIWEI1125 <34065410+KAIWEI1125@users.noreply.github.com>
2018-08-23 22:36:57 +0800

    Add files via upload

---
+++ GuestbookView.cs
@@ -0,0 +1,29 @@
+﻿using GaryGuestbook.Models;
+using GaryGuestbook.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace GaryGuestbook.ViewModels
+{
+    public class GuestbookView
+    {
+        /// <summary>
+        /// 搜尋欄位
+        /// </summary>
+        [DisplayName("搜尋:")]
+        public string Search { get; set; }
+
+        /// <summary>
+        /// 顯示Guestbooks的List
+        /// </summary>
+        public List<Guestbooks> DataList { get; set; }
+
+        /// <summary>
+        /// 分頁內容
+        /// </summary>
+        public ForPaging Paging { get; set; }
+    }
+}
